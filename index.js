790e7ebca7a310ea8af377172997879864e592fc
lollwhat <leechenwei7@gmail.com>
2025-08-08 23:09:27 +0800

    update

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 mongoose.connect(
-"mongodb+srv://<name>:<password>@cluster0.38u1b.mongodb.net/todoDB");
+"mongodb+srv://brandonlcw2004:<db_password>@cluster0.tg7u4me.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 
 const taskSchema = {
     name: {
@@ -22,3 +22,51 @@ const taskSchema = {
 };
 
 const Task = mongoose.model("Task", taskSchema);
+
+app.get("/", function(req, res){
+    let today = new Date();
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    let day = today.toLocaleDateString("en-US", options);
+
+    Task.find({}, function(err, foundTasks){
+        if (!err) {
+            res.render("list", { tasks: foundTasks });
+        }else{
+            console.log(err);
+        }
+    });
+});
+
+app.post("/", function(req, res){
+    const taskName = req.body.newTask;
+    if(taskName){
+        const task = new Task({
+            name: taskName,
+        });
+        task.save()
+            .then(() => {
+                res.redirect("/");
+            });
+    }else{
+        res.redirect("/");
+    }
+})
+
+app.post("/delete", function(req,res){
+    const checkedItemId = req.body.checkbox;
+    Task.findByIdAndRemove(checkedItemId, function(err){
+        if(!err){
+            console.log("Successfully deleted the task.");
+            res.redirect("/");
+        }
+    });
+});
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Server running on port 3000");
+});
